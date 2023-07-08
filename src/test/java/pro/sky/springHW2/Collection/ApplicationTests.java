6fbceec5c75a554743077e47ff1eeffc1e54beb9
Fet2113 <super.gjoll@yandex.ru>
2023-07-08 15:12:10 +0300

    Initial commit

---
+++ src/test/java/pro/sky/springHW2/Collection/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.springHW2.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
